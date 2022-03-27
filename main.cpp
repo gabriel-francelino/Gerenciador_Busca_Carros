d9a36b083edd23db7886678905dbcd5fc6482096
gabriel-francelino <90209521+gabriel-francelino@users.noreply.github.com>
2022-03-27 14:07:06 -0300

    Update main.cpp
    Funcionando

--- main.cpp
+++ main.cpp
@@ -1,40 +1,38 @@
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
-#include <string.h>
 
 #include "funcoes.h"
-#include "pilha.h"
-#include "fila.h"
 
 using namespace std;
 
 void interface() {
-    cout << "+---------------------------------------+" << endl;
-    cout << "|       GERENCIAMENTO DE BUSCA          |" << endl;
-    cout << "|   Escolha qual comando deseja fazer:  |" << endl;
-    cout << "|     1)Adicinar veículo.               |" << endl;
-    cout << "|     2)Remover veículo.                |" << endl;
-    cout << "|     3)Busca 1.                        |" << endl;
-    cout << "|     4)Busca 2.                        |" << endl;
-    cout << "|     5)Relatório.                      |" << endl;
-    cout << "|     6)Finalizar.                      |" << endl;
-    cout << "+---------------------------------------+" << endl;
+    cout << endl;
+    cout << "+-------------------------------------------+" << endl;
+    cout << "|         GERENCIAMENTO DE BUSCA            |" << endl;
+    cout << "|     Escolha qual comando deseja fazer:    |" << endl;
+    cout << "|       1)Adicinar veiculo.                 |" << endl;
+    cout << "|       2)Remover veiculo.                  |" << endl;
+    cout << "|       3)Busca (Ano <= 2015 - Pilha).      |" << endl;
+    cout << "|       4)Busca (Km < 20.000 - Fila).       |" << endl;
+    cout << "|       5)Relatorio.                        |" << endl;
+    cout << "+-------------------------------------------+" << endl;
 }
 
 // Variaveis
-//Pilha *pilha;
-//Fila *fila;
-
+tPilha *pilha;
+tFila *fila;
 tLista *lista;
 
 // ------------------------------------------------------------------
 
 int main(int argc, char** argv) {
     int op;
-    ifstream cadastro("BD-int.txt");
+    ifstream cadastro("BD_veiculos.txt");
     if (cadastro.is_open()) {
         lista = inicia_lista();
+        pilha = inicia_pilha();
+        fila = inicia_fila();
         while (!cadastro.eof()) {
             no *dados = new(no);
             cadastro >> dados->modelo;
@@ -55,111 +53,143 @@ int main(int argc, char** argv) {
         }
         cadastro.close();
     } else {
-        cout << "Arquivo não encontrado!" << endl;
+        cout << "Arquivo nao encontrado!" << endl;
         delete(lista);
         return 0;
     }
-
-    // Daqui pra cima no main ta funcionando
-    // ------------------------------------------------
-    no* ptr = lista->lista;
-    tPilha* pilha;
-    tFila* fila;
     do {
         interface();
-        cout << "Digite o comando: ";
+        cout << "Digite a opcao que deseja realizar: ";
         cin >> op;
-        cout << endl << "+---------------------------------------+" << endl;
+        cout << endl;
         switch (op) {
             case 1:
-                cout << "Adicionar Veículo" << endl;
+            {
+                cout << "Digite as informacoes do veiculo:" << endl << endl;
                 no veiculo;
 
-                cout << "Digite as informações do veículo:" << endl;
+                // cout << "Modelo do veiculo: ";
+                // cin.ignore();
+                // getline(cin, veiculo.modelo);
 
-                // Fazer leitura do teclado dos dados e tratamento
+                // cout << "Marca do veiculo: ";
+                // getline(cin, veiculo.marca);
 
-                veiculo.modelo = 14;
-                veiculo.marca = 4444;
-                veiculo.tipo = 11;
-                veiculo.tipo = 2015;
-                veiculo.km = 23412;
-                veiculo.potencia = 1.0;
-                veiculo.combustivel = 1;
-                veiculo.cambio = 1;
-                veiculo.direcao = 2;
-                veiculo.cor = 40;
-                veiculo.nPortas = 2;
-                veiculo.placa = 7865;
+                // cout << "Tipo do veiculo: ";
+                // getline(cin, veiculo.tipo);
+
+                // cout << "Ano do veiculo: "; //INTEIRO
+                // cin >> veiculo.ano;
+
+                // cout << "Km do veiculo: "; //INTEIRO
+                // cin >> veiculo.km;
+
+                // cout << "Potencia do veiculo: "; //FLOAT
+                // cin >> veiculo.potencia;
+
+                // cout << "Combustivel do veiculo: ";
+                // cin.ignore();
+                // getline(cin, veiculo.combustivel);
+
+                // cout << "Cambio do veiculo: ";
+                // getline(cin, veiculo.cambio);
+
+                // cout << "Direcao do veiculo: ";
+                // getline(cin, veiculo.direcao);
+
+                // cout << "Cor do veiculo: ";
+                // getline(cin, veiculo.cor);
+
+                // cout << "Numero de portas do veiculo: "; // INTEIRO
+                // cin >> veiculo.nPortas;
+
+                // cout << "Placa do veiculo: ";
+                // cin.ignore();
+                // getline(cin, veiculo.placa);
+                // cout << endl;
+
                 veiculo.prox = NULL;
 
+                veiculo.modelo = "City";
+                veiculo.marca = "Honda";
+                veiculo.tipo = "Sedan";
+                veiculo.ano = 2014;
+                veiculo.km = 18000;
+                veiculo.potencia = 1.5;
+                veiculo.combustivel = "Flex";
+                veiculo.cambio = "Automatico";
+                veiculo.direcao = "Hidraulica";
+                veiculo.cor = "Prata";
+                veiculo.nPortas = 4;
+                veiculo.placa = "AA7865";
+
                 if (insere_enc(lista, veiculo.placa, &veiculo)) {
-                    cout << "Adicionado com Sucesso!" << endl;
+                    cout << "Adicionado com sucesso!" << endl;
                 } else {
                     cout << "Falha ao adicionar." << endl;
                 }
-                break;
+            }
+            break;
             case 2:
-                cout << "Remover Veículo" << endl;
-                cout << "Digite a placa do Veículo que será removida: ";
-                int placa;
-                placa = 4572; // cin >> placa;
+            {
+                cout << "Digite a placa do veiculo que sera removida: ";
+                string placa;
+
+                cin.ignore();
+                getline(cin, placa);
                 cout << endl;
 
                 if (remove_enc(lista, placa)) {
-                    cout << "Removido com sucesso" << endl;
+                    cout << "Removido com sucesso!" << endl;
                 } else {
-                    cout << "Falha ao remover" << endl;
+                    cout << "Falha ao remover." << endl;
                 }
-                break;
+            }
+            break;
             case 3:
-                cout << "Busca por veiculos ano 2019 ou menos. (Pilha)" << endl;
-                
-                pilha = inicia_pilha();
-
-                if (ptr == NULL) {
-                    cout << "A lista está vazia!!" << endl;
-                } else {
-                    while (ptr != NULL) {
-                        if (ptr->ano <= 2019) {
-                            insere_pilha(pilha, ptr);
-                        }
-                        ptr = ptr->prox;
+            {
+                cout << "Busca por veiculos ano 2015 ou menos. (Pilha)" << endl;
+                no *ptr = lista->lista;
+                while (ptr != NULL) {
+                    if (ptr->ano <= 2015) {
+                        insere_pilha(&ptr, pilha);
                     }
-                    imprime(pilha);
+                    ptr = ptr->prox;
                 }
-                break;
+                imprime_pilha(pilha);
+            }
+            break;
             case 4:
-                cout << "Busca por veículo com menos de 40.000km (Fila)" << endl;
-                
-                fila = inicia_fila();
-
-                if (ptr == NULL) {
-                    cout << "A lista está vazia!!" << endl;
-                } else {
-                    while (ptr != NULL) {
-                        if (ptr->km < 40000) {
-                            insere_fila(fila, ptr);
-                        }
-                        ptr = ptr->prox;
+            {
+                cout << "Busca por veiculo com menos de 20.000km (Fila)" << endl;
+                no *ptr = lista->lista;
+                while (ptr != NULL) {
+                    if (ptr->km < 20000) {
+                        insere_fila(&ptr, fila);
                     }
-
-                    imprime(fila);
+                    ptr = ptr->prox;
                 }
-                break;
+                imprime_fila(fila);
+            }
+            break;
             case 5:
-                cout << "Relatório: " << endl;
+            {
+                cout << "Relatorio: " << endl;
                 imprime(lista);
-                break;
-            case 6:
-                cout << "Finalizar Programa" << endl;
-                break;
+            }
+            break;
             default:
-                cout << "Opcao Inválida" << endl;
-                interface();
-                break;
+            {
+                encerra_pilha(pilha);
+                encerra_fila(fila);
+                encerra_lista(lista);
+                cout << "+-------------------------------------------+" << endl;
+                cout << "|      Programa finalizado com sucesso!!    |" << endl;
+                cout << "+-------------------------------------------+" << endl;
+                op = 6;
+            }
+            break;
         }
     } while (op != 6);
-
     return 0;
 }
